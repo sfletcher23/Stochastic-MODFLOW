a599ccdbe0ff1671a2820279189a7e0b14fa21a8
Sarah Fletcher <sfletch@mit.edu>
2017-05-08 14:24:55 -0400

    saves output in .mat file

--- analyzeHeads.py
+++ analyzeHeads.py
@@ -14,6 +14,7 @@ import subprocess
 import matplotlib.pyplot as plt
 import shutil
 from scipy.stats import uniform
+import scipy.io as io
 
 
 # Write function to generate range of inputs for hk, vka, pumping rate in each well
@@ -45,7 +46,7 @@ def genParamSamples(hk_min, hk_max, vka_min, vka_max, numWells, pump_min, pump_m
 
 
 # Plot settings
-plotContours = False
+plotContours = True
 plotHydrograph = True
 
 
@@ -76,6 +77,8 @@ sy = 2.5e-1
 ss = 4.e-7
 laytyp = 1  # 1 = unconfined, 0 = confined
 hdry = 0    # dry cell head set to this number
+mxiter = 100
+hclose = 1e-3
 
 # Variables for the BAS package
 ibound = np.ones((nlay, nrow, ncol), dtype=np.int32)    # Not sure if GW uses 0 or 1
@@ -84,7 +87,7 @@ strt = 1000 * np.ones((nlay, nrow, ncol), dtype=np.float32)     # Starting head
 # Time step parameters
 nper = 1
 perlen = [3650]
-nstp = [40]
+nstp = 50
 steady = [False]
 
 # Well locations
@@ -92,13 +95,24 @@ wpt1 = (0, 20-1, 37-1)
 wpt2 = (0, 37-1, 18-1)
 
 # Output control
-spd = {(0, 0): ['print head', 'save head', 'print budget', 'save budget']}
+spd = {(0, 0): ['print head', 'save head', 'print budget', 'save budget', 'print drawdown', 'save drawdown']}
 
 
 # Get variable inputs
 samples = genParamSamples(hk_min=hk_min, hk_max=hk_max,vka_min=vka_min, vka_max=vka_max, numWells=numWells,
                           pump_min=pump_min, pump_max=-pump_max, sampleSize=sampleSize)
 
+
+# Define output parameters for each run
+model_name = []
+for i in range(sampleSize):
+    model_name.append('model' + str(i))
+modflow_output = []
+modflow_succes = []
+head_object = []
+head_data1 = np.zeros([sampleSize, nstp])
+head_data2 = np.zeros([sampleSize, nstp])
+    
 # Run MODFLOW for each of the paramter samples
 for i in range(sampleSize):
 
@@ -114,14 +128,13 @@ for i in range(sampleSize):
     vka = samples['vka'][i]
 
     # Flopy objects
-    modelname = 'TestFlopy'
-    mf = flopy.modflow.Modflow(modelname, exe_name='./mf2005dbl')
+    mf = flopy.modflow.Modflow(model_name[i], exe_name='./mf2005dbl')
     dis = flopy.modflow.ModflowDis(mf, nlay, nrow, ncol, delr=delr, delc=delc,
                                    top=ztop, botm=botm[1:],
                                    nper=nper, perlen=perlen, nstp=nstp, steady=steady)
     bas = flopy.modflow.ModflowBas(mf, ibound=ibound, strt=strt)
     lpf = flopy.modflow.ModflowLpf(mf, hk=hk, vka=vka, sy=sy, ss=ss, laytyp=laytyp, hdry=hdry)
-    pcg = flopy.modflow.ModflowPcg(mf)  # This has stuff like iterations for solver
+    pcg = flopy.modflow.ModflowPcg(mf, mxiter=mxiter, hclose=hclose)  # This has stuff like iterations for solver
     wel = flopy.modflow.ModflowWel(mf, stress_period_data=stress_period_data)
     oc = flopy.modflow.ModflowOc(mf, stress_period_data=spd)
 
@@ -130,26 +143,30 @@ for i in range(sampleSize):
 
     # Run the model
     success, mfoutput = mf.run_model(silent=True, pause=False, report=True)
-    if not success:
-        raise Exception('MODFLOW did not terminate normally.')
+    modflow_output.append(mfoutput)
+    modflow_succes.append(success)
+    # if not success:
+    #     raise Exception('MODFLOW did not terminate normally.')
 
 
     # Create the headfile object
-    headobj = bf.HeadFile(modelname+'.hds')
+    headobj = bf.HeadFile(model_name[i]+'.hds')
+    head_object.append(headobj)
     time = headobj.get_times()
 
     # Plot hydrograph: head versus time
     if plotHydrograph:
         ts1 = headobj.get_ts(wpt1)
         ts2 = headobj.get_ts(wpt2)
-        plt.subplot(1, 1, 1)
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(111)
         ttl = 'Head at cell ({0},{1},{2})'.format(wpt1[0] + 1, wpt1[1] + 1, wpt1[2] + 1)
-        plt.title(ttl)
-        plt.xlabel('time')
-        plt.ylabel('head')
-        plt.plot(ts1[:, 0], ts1[:, 1])
-        plt.plot(ts2[:, 0], ts2[:, 1])
-        plt.show()
+        ax1.set_title(ttl)
+        ax1.set_xlabel('time')
+        ax1.set_ylabel('head')
+        ax1.plot(ts1[:, 0], ts1[:, 1])
+        ax1.plot(ts2[:, 0], ts2[:, 1])
+        fig1.savefig('hydrograph' + str(i) + '.pdf')
 
     # Plot contour map
     if plotContours:
@@ -157,34 +174,42 @@ for i in range(sampleSize):
         # Setup contour parameters
         levels = np.arange(1, 1010, 20)
         extent = (delr/2., Lx - delr/2., delc/2., Ly - delc/2.)
-        print('Levels: ', levels)
-        print('Extent: ', extent)
-        print('Times: ', time)
 
         # Make the plots
-        mytimes = [time[1], time[30]]
+        mytimes = [time[-1]]
         for iplot, t in enumerate(mytimes):
-            print('*****Processing time: ', t)
             head = headobj.get_data(totim=t)
-            #Print statistics
-            print('Head statistics')
-            print('  min: ', head.min())
-            print('  max: ', head.max())
-            print('  std: ', head.std())
 
         #Create the plot
-            #plt.subplot(1, len(mytimes), iplot + 1, aspect='equal')
-            plt.subplot(1, 1, 1, aspect='equal')
-            plt.title('stress period ' + str(iplot + 1))
-            plt.imshow(head[0, :, :], extent=extent, cmap='BrBG', vmin=0., vmax=10.)
-            plt.colorbar()
+            fig2 = plt.figure()
+            ax2 = fig2.add_subplot(1, 1, 1, aspect='equal')
+            ax2.set_title('stress period ' + str(iplot + 1))
+            ax2.imshow(head[0, :, :], extent=extent, cmap='BrBG', vmin=0., vmax=10.)
             CS = plt.contour(np.flipud(head[0, :, :]), levels=levels, extent=extent,
                                 zorder=10)
             plt.clabel(CS, inline=1, fontsize=10, fmt='%1.1f', zorder=11)
+            plt.colorbar()
             mfc = 'None'
             if (iplot+1) == len(mytimes):
                 mfc='black'
-            plt.show()
-        plt.show()
+            plt.savefig('contour' + str(i) + '.pdf')
+
+    file = open('modflow_output' + str(i) + '.txt', 'w')
+
+    # Create MODFLOW output file if there was an error
+    for n in modflow_output[i]:
+        if not success:
+            file.write(n + '\n')
+
+    # Write hydrograph data to array
+    head_data1[i, :] = ts1[:, 1]
+    head_data2[i, :] = ts2[:, 1]
+
+
+# Save output in .mat file
+outputDic = dict(zip(['head_data1', 'head_data2', 'time'], [head_data1, head_data2, time]))
+outputDic.update(samples)
+io.savemat('modflowData.mat', outputDic)
+
 
 
