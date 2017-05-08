c478c4c87eb9575cdfe9cd93714cb6a8e1a59c2e
Sarah Fletcher <sfletch@mit.edu>
2017-05-08 16:03:16 -0400

    Parameter generator is stochastic, output works

--- analyzeHeads.py
+++ analyzeHeads.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
 import shutil
 from scipy.stats import uniform
 import scipy.io as io
+import datetime
+from scipy.stats import uniform
 
 
 # Write function to generate range of inputs for hk, vka, pumping rate in each well
@@ -25,13 +27,16 @@ def genParamSamples(hk_min, hk_max, vka_min, vka_max, numWells, pump_min, pump_m
         params_in_sample.append('pump_rate_' + str(n + 1))
 
     # Generate arrays of hk and vka
-    hk = np.linspace(hk_min, hk_max, num=sampleSize)
-    vka = np.linspace(vka_min, vka_max, num=sampleSize)
+    scale = hk_max - hk_min
+    hk = uniform.rvs(loc=hk_min, scale=scale, size=sampleSize)
+    scale = vka_max - vka_min
+    vka = uniform.rvs(loc=hk_min, scale=scale, size=sampleSize)
 
     # Generate arrays of pumping rate
     pump = np.zeros([numWells, sampleSize])
+    scale = pump_max - pump_min
     for n in range(numWells):
-        pump[n, :] = np.linspace(pump_min, pump_max, num=sampleSize)
+        pump[n, :] = uniform.rvs(pump_min, scale, size=sampleSize)
 
     # Combine to form paramSample
     pumpSplit = np.split(pump, numWells)
@@ -39,16 +44,18 @@ def genParamSamples(hk_min, hk_max, vka_min, vka_max, numWells, pump_min, pump_m
     for i in range(numWells):
         param_sample = np.append(param_sample, pumpSplit[i], axis=0)
 
-    # Create dictionary with samples for each paramter
+    # Create dictionary with samples for each parameter
     params = dict(zip(params_in_sample, param_sample))
 
     return params
 
 
 # Plot settings
-plotContours = True
-plotHydrograph = True
+plotContours = False
+plotHydrograph = False
 
+# Delete modflow files after use?
+deleteFiles = True
 
 # Parameter sample inputs
 hk_min = 1.e-3
@@ -58,7 +65,7 @@ vka_max = 1.e-2
 numWells = 2
 pump_min = -50000.
 pump_max = -3000.
-sampleSize = 10
+sampleSize = 100
 
 # Fixed Parameter Definitions
 # Model domain and grid definition
@@ -77,8 +84,8 @@ sy = 2.5e-1
 ss = 4.e-7
 laytyp = 1  # 1 = unconfined, 0 = confined
 hdry = 0    # dry cell head set to this number
-mxiter = 100
-hclose = 1e-3
+mxiter = 200
+hclose = 1e-1
 
 # Variables for the BAS package
 ibound = np.ones((nlay, nrow, ncol), dtype=np.int32)    # Not sure if GW uses 0 or 1
@@ -87,7 +94,7 @@ strt = 1000 * np.ones((nlay, nrow, ncol), dtype=np.float32)     # Starting head
 # Time step parameters
 nper = 1
 perlen = [3650]
-nstp = 50
+nstp = 100
 steady = [False]
 
 # Well locations
@@ -95,7 +102,7 @@ wpt1 = (0, 20-1, 37-1)
 wpt2 = (0, 37-1, 18-1)
 
 # Output control
-spd = {(0, 0): ['print head', 'save head', 'print budget', 'save budget', 'print drawdown', 'save drawdown']}
+spd = {(0, 0): ['print head', 'save head']}
 
 
 # Get variable inputs
@@ -104,17 +111,16 @@ samples = genParamSamples(hk_min=hk_min, hk_max=hk_max,vka_min=vka_min, vka_max=
 
 
 # Define output parameters for each run
-model_name = []
-for i in range(sampleSize):
-    model_name.append('model' + str(i))
-modflow_output = []
-modflow_succes = []
+modflow_success = []
 head_object = []
 head_data1 = np.zeros([sampleSize, nstp])
 head_data2 = np.zeros([sampleSize, nstp])
     
 # Run MODFLOW for each of the paramter samples
 for i in range(sampleSize):
+
+    # Name model this run
+    model_name = 'model' + str(i)
 
     # Get pumping rate same and make stress period dictionary
     pumping_rate1 = samples['pump_rate_1'][i]
@@ -128,7 +134,7 @@ for i in range(sampleSize):
     vka = samples['vka'][i]
 
     # Flopy objects
-    mf = flopy.modflow.Modflow(model_name[i], exe_name='./mf2005dbl')
+    mf = flopy.modflow.Modflow(model_name, exe_name='./mf2005dbl')
     dis = flopy.modflow.ModflowDis(mf, nlay, nrow, ncol, delr=delr, delc=delc,
                                    top=ztop, botm=botm[1:],
                                    nper=nper, perlen=perlen, nstp=nstp, steady=steady)
@@ -142,22 +148,20 @@ for i in range(sampleSize):
     mf.write_input()
 
     # Run the model
-    success, mfoutput = mf.run_model(silent=True, pause=False, report=True)
-    modflow_output.append(mfoutput)
-    modflow_succes.append(success)
-    # if not success:
-    #     raise Exception('MODFLOW did not terminate normally.')
-
+    success, modflow_output = mf.run_model(silent=True, pause=False, report=True)
+    modflow_success.append(success)
 
     # Create the headfile object
-    headobj = bf.HeadFile(model_name[i]+'.hds')
+    headobj = bf.HeadFile(model_name+'.hds')
     head_object.append(headobj)
     time = headobj.get_times()
 
+    # Get hydrograph data
+    ts1 = headobj.get_ts(wpt1)
+    ts2 = headobj.get_ts(wpt2)
+
     # Plot hydrograph: head versus time
     if plotHydrograph:
-        ts1 = headobj.get_ts(wpt1)
-        ts2 = headobj.get_ts(wpt2)
         fig1 = plt.figure()
         ax1 = fig1.add_subplot(111)
         ttl = 'Head at cell ({0},{1},{2})'.format(wpt1[0] + 1, wpt1[1] + 1, wpt1[2] + 1)
@@ -194,22 +198,35 @@ for i in range(sampleSize):
                 mfc='black'
             plt.savefig('contour' + str(i) + '.pdf')
 
-    file = open('modflow_output' + str(i) + '.txt', 'w')
-
     # Create MODFLOW output file if there was an error
-    for n in modflow_output[i]:
-        if not success:
+    if not success:
+        file = open('modflow_output' + str(i) + '.txt', 'w')
+        for n in modflow_output:
             file.write(n + '\n')
+        raise Warning('MODFLOW did not terminate normally.')
 
     # Write hydrograph data to array
     head_data1[i, :] = ts1[:, 1]
     head_data2[i, :] = ts2[:, 1]
 
+    if deleteFiles:
+        os.remove(model_name + '.bas')
+        os.remove(model_name + '.dis')
+        os.remove(model_name + '.hds')
+        os.remove(model_name + '.list')
+        os.remove(model_name + '.lpf')
+        os.remove(model_name + '.nam')
+        os.remove(model_name + '.oc')
+        os.remove(model_name + '.pcg')
+        os.remove(model_name + '.wel')
+
 
 # Save output in .mat file
-outputDic = dict(zip(['head_data1', 'head_data2', 'time'], [head_data1, head_data2, time]))
+outputDic = dict(zip(['head_data1', 'head_data2', 'time', 'modflow_success'],
+                     [head_data1, head_data2, time, modflow_success]))
 outputDic.update(samples)
-io.savemat('modflowData.mat', outputDic)
+datetimeStr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+io.savemat('modflowData' + datetimeStr, outputDic)
 
 
 
