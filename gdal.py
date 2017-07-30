11995a4b65d5b2a9e5521bc91a48f8219b72fd0f
Sarah Fletcher <sfletch@mit.edu>
2017-07-30 11:12:40 -0400

    Cleaned up old data files and scripts

--- gdal.py
+++
@@ -1,7 +0,0 @@
-import flopy
-import os
-from osgeo import goal
-
-if os.path.exits("dem.tiff"):
-    modName = 'mod'
-    mf1 = flopy.modflow.Modflow(modName, exe_name=   )
--- sampleCodeSnippets.py
+++
@@ -1,6 +0,0 @@
-
-
-# Draw samples from uniform distribution
-from scipy.stats import uniform
-scale = hk_max - hk_min
-hk_sample = uniform.rvs(loc=hk_min, scale=scale, size=size)
